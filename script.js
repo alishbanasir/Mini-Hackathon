0c38f464fa7e79a6d7504a76f792a339fa370973
Alishba Nasir <165299398+alishbanasir@users.noreply.github.com>
2025-01-04 23:04:12 +0500

    update

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let orders = [];
 
-// Add item to the cart
+
 function addToCart(productName) {
     orders.push(productName);
     alert(`${productName} added to your orders!`);
 }
 
-// Show the order popup
+
 document.getElementById('view-orders').addEventListener('click', () => {
     const orderList = document.getElementById('order-list');
     orderList.innerHTML = '';
@@ -24,7 +24,11 @@ document.getElementById('view-orders').addEventListener('click', () => {
     document.getElementById('order-popup').classList.remove('hidden');
 });
 
-// Close the order popup
+
 function closePopup() {
     document.getElementById('order-popup').classList.add('hidden');
 }
+
+
+
+
